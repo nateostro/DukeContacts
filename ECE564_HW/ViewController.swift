395b9723fcd365f1edf6d85b03673027e813808c
rt113@duke.edu <rt113@duke.edu>
2020-05-11 15:28:42 -0400

    Initial Commit

---
+++ ECE564_HW/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ECE564_HW
+//
+//  Created by Ric Telford on 5/11/20.
+//  Copyright © 2020 ECE564. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
